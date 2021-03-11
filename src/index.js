4934e8473ebc7d76c4b2adbf418a98412fd5f3c5
Dat Do <gabrielda3108@gmail.com>
2021-03-11 19:24:39 +0700

    add more dependencies

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log();
+  console.log(`Server listening on port ${port}`);
 });
